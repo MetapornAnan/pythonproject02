1521af5d66736f9aef2d610ad8fcfbad9b52397a
MetapornAnan <s6519410054@sau.ac.th>
2023-08-25 12:05:30 +0700

    code V.3 : create test04 Khao

--- test02.py
+++ test02.py
@@ -8,5 +8,5 @@ print("Hello... {} Hi... {} {} Ola...".format(456,True,10+20))
 print("Hello... {2} Hi... {0} {1} Ola...".format(456,True,10+20)) #index number เริ่มจาก 0 สลับตำแหน่งได้
 #ใช้ f-string ใส่ f ข้างหน้า str ใส่ในปีกกา
 print(f"Hello... {456} Hi... {True} {10+20} Ola...")
-#ใช้ modulas operater (%) -> %dจำนวนเต็ม, %f, %c, %sค่าTrueFalse,%iค่าTrueFalse แต่เป็น 1 กับ 0 .....
+#ใช้ modulas operater (%) -> %dจำนวนเต็ม, %f, %c, %sค่าTrueFalse,%iค่าTrueFalse แต่เป็น 1 กับ 0,%sใช้กับ str .....
 print("Hello...%d Hi... %s %d Ola..." %(456,True,10+20) )
